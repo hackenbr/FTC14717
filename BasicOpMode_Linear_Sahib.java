9f9433e78e172b21246d56b56b7192c3f4bb8030
sahib <44450660+POKEPOWA@users.noreply.github.com>
2018-12-13 19:56:38 -0700

    Add files via upload

--- BasicOpMode_Linear_Sahib.java
+++ BasicOpMode_Linear_Sahib.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -6,33 +6,16 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.hardware.Servo;
 
-
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+@TeleOp(name="sahibs code 2 lol", group="Linear Opmode")
 public class BasicOpMode_Linear_Sahib extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
-    private DcMotor arm = null;
-    private DcMotor sweeper = null;
-    private Servo bucket = null;
-    private double bucketPosition =0;
+    private DcMotor hook = null;
+
 
     @Override
     public void runOpMode() {
@@ -44,15 +27,13 @@ public class BasicOpMode_Linear_Sahib extends LinearOpMode {
         // step (using the FTC Robot Controller app on the phone).
         leftDrive  = hardwareMap.get(DcMotor.class, "motor0");
         rightDrive = hardwareMap.get(DcMotor.class, "motor1");
-        arm = hardwareMap.get(DcMotor.class, "motor2");
-        sweeper = hardwareMap.get(DcMotor.class, "motor3");
-        bucket = hardwareMap.get(Servo.class, "bucket");
+        hook = hardwareMap.get(DcMotor.class, "motor2");
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
-        arm.setDirection(DcMotor.Direction.FORWARD);
-        sweeper.setDirection(DcMotor.Direction.FORWARD);
+        hook.setDirection(DcMotor.Direction.FORWARD);
+
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -64,7 +45,7 @@ public class BasicOpMode_Linear_Sahib extends LinearOpMode {
             // Setup a variable for each drive wheel to save power level for telemetry
             double leftPower;
             double rightPower;
-            double armPower;
+
 
 
             // Choose to drive using either Tank Mode, or POV Mode
@@ -72,15 +53,13 @@ public class BasicOpMode_Linear_Sahib extends LinearOpMode {
 
             // POV Mode uses left stick to go forward, and right stick to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = gamepad1.left_stick_y;
-            double turn  =  -gamepad1.left_stick_x/2;
-            double armMove = -gamepad1.right_stick_y;
+            double drive = -gamepad1.left_stick_y;
+            double turn  =  -gamepad1.left_stick_x;
             double turbo = gamepad1.right_trigger+1;
 
 
             leftPower    = Range.clip(drive + turn, -1.1*turbo, 1.1*turbo) ;
             rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-            armPower     = Range.clip(armMove, -1.0, 1.0);
             // Tank Mode uses one stick to control each wheel.
             // - This requires no math, but it is hard to drive forward slowly and keep straight.
             // leftPower  = -gamepad1.left_stick_y ;
@@ -89,29 +68,23 @@ public class BasicOpMode_Linear_Sahib extends LinearOpMode {
             // Send calculated power to wheels
             leftDrive.setPower(leftPower);
             rightDrive.setPower(rightPower);
-            arm.setPower(armPower);
 
-            if (gamepad1.left_bumper) {
-                sweeper.setPower(1);
+            if (gamepad1.y) {
+                hook.setPower(1);
             }
-            else if (gamepad1.right_bumper) {
-                sweeper.setPower(-1);
+
+            else if (gamepad1.a) {
+                hook.setPower(-1);
             }
+
             else {
-                sweeper.setPower(0);
-
+                hook.setPower(0);
             }
-            if (gamepad1.a && bucketPosition<=0) {
-                bucketPosition += 0.005;
-            }
-            else if(gamepad1.b && bucketPosition>=1) {
-                bucketPosition -= 0.005;
-            }
-            bucket.setPosition(bucketPosition);
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.addData("name:", "sahib");
+            telemetry.addData("Hook height:", hook.getCurrentPosition());
             telemetry.update();
         }
     }
